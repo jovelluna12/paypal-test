26e3368402317b36f23d4834663f881d47ce1f5e
jovelluna12 <jchrister718@gmail.com>
2024-02-09 19:02:46 +0800

    paypal integration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <div id="paypal-button-container"></div>
     <p id="result-message"></p>
     <!-- Replace the "test" client-id value with your client-id -->
-    <script src="https://www.paypal.com/sdk/js?client-id=AVnwf7qg_UQV8tXGqkowoloX39vi50OU4CHiaIo6b43rwc8CgkfOWiymICQbnLQMbKOPziSnlxhHI95S&components=buttons&enable-funding=paylater,venmo,card" data-sdk-integration-source="integrationbuilder_sc"></script>
+    <script src="https://www.paypal.com/sdk/js?client-id=REPLACE ME WITH LIVE CLIENT ID&components=buttons&enable-funding=paylater,venmo,card" data-sdk-integration-source="integrationbuilder_sc"></script>
     <script src="{{asset('js/app.js')}}"></script>
   </body>
 </html>
